53708e4f6ac70be5894d130e9a4751523efb4eb7
pav-suv <rpavana04@gmail.com>
2026-01-31 17:03:28 +0530

    03-basic file has been added

---
+++ 03-Basic_WithVariableAndFunction.ps1
@@ -0,0 +1,14 @@
+﻿
+function cf {
+
+$FolderName = "MyAutomation"
+$RootFolderName = 'C:\Automation\Command\'
+
+# Programing section
+Set-Location $RootFolderName
+New-Item -Name $FolderName -Path C:\Automation\Command\ -ItemType Directory
+
+}
+
+
+cf 
