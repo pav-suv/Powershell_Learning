85803ae8dde0ef918f658eae61e5bb162871f268
pav-suv <rpavana04@gmail.com>
2026-02-01 02:23:50 +0530

    One line added at the end and deleted from middle 'md myfolderA'

--- 01-Basic.ps1
+++ 01-Basic.ps1
@@ -2,7 +2,7 @@
 # Name of developer pvn
 # Date 31Jan2026
 Set-Location 'C:\Automation\Command\'
-md myfolderA
 rd .\myfolderA\
 New-Item -Name MyFolderA -Path C:\Automation\Command\ -ItemType Directory
 Remove-Item .\MyFolderA
+md myfolderA
