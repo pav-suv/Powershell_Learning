e97e58aafcc62f062ae071cc0e4627a098687ebe
pav-suv <rpavanao4@gmail.com>
2026-02-03 12:07:50 +0530

    added a new file

---
+++ 04-Basic_WaysUsedToWriteParameters.ps1
@@ -0,0 +1,23 @@
+﻿
+# Option 1
+
+param (
+[Parameter(Mandatory=$true)]
+[string]$FolderName,
+[Parameter(Mandatory=$true)]
+[string]$RootFolderName,
+[Parameter(Mandatory=$false)]
+[string]$FileName,
+[Parameter(Mandatory=$true)]
+[string]$FilePath
+)
+
+
+########## Option 2
+
+param (
+[Parameter(Mandatory=$true)][string]$FolderName,
+[Parameter(Mandatory=$true)][string]$RootFolderName,
+[Parameter(Mandatory=$false)][string]$FileName,
+[Parameter(Mandatory=$true)][string]$FilePath
+)
